b2a0a58930fb50593effa44bda617e0d347d115e
Wexxxxxxx <denmarkdorado20@gmail.com>
2024-07-02 16:46:22 +0800

    updated

--- frontend/src/pages/Player.jsx
+++ frontend/src/pages/Player.jsx
@@ -5,9 +5,6 @@ import Img3 from "../assets/img/varsity6.jpg";
 import Img4 from "../assets/img/varsity7.jpg";
 import Img5 from "../assets/img/varsity8.jpg";
 import Img6 from "../assets/img/varsity1.jpg";
-import Img7 from "../assets/img/varsity2.jpg";
-import Img8 from "../assets/img/varsity3.jpg";
-import Img10 from "../assets/img/varsity11.jpg";
 
 const Player = () => {
   return (
